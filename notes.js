26e80c31ef5a5eda1a6ded6965a4100236291d25
musikele <michele.nasti@gmail.com>
2017-02-01 17:49:38 +0100

    added yargs

--- notes.js
+++ notes.js
@@ -1,3 +1,43 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
 
+var addNote = (title, body) => {
+
+	let notes = [];
+	var note = {
+		title,
+		body
+	};
+
+	try {
+		let notesString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(notesString);
+	} catch (e) {	}
+
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	}
+};
+
+var getAll = () => {
+	console.log("Getting all notes")
+}
+
+var getNote = (title) => {
+	console.log("Getting note", title);
+}
+
+var removeNote = (title) => {
+	console.log("Removing note", title);
+}
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
+};
